0a1033c80c6b78f266b65074b9792a82bdfb8965
drigobl <157318201+drigobl@users.noreply.github.com>
2025-01-20 16:08:49 -0500

    Update server configuration for Deno Deploy

--- main.ts
+++ main.ts
@@ -1,2 +1,7 @@
-console.log("Hello, World!");
-Deno.serve(() => new Response("Hello, world!"));
+Deno.serve((_req) => new Response("Hello, world!"), {
+    port: Number(Deno.env.get("PORT")) || 8000,
+    hostname: "0.0.0.0",
+    onListen: ({ port, hostname }) => {
+      console.log(`Server started at http://${hostname}:${port}`);
+    },
+  });
