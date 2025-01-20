7ba6a83ab2d838843c3de0461f251a5a19fe006b
drigobl <157318201+drigobl@users.noreply.github.com>
2025-01-20 16:20:38 -0500

    Simplify server configuration

--- main.ts
+++ main.ts
@@ -1,7 +1,4 @@
 Deno.serve((_req) => new Response("Hello, world!"), {
-    port: Number(Deno.env.get("PORT")) || 8000,
-    hostname: "0.0.0.0",
-    onListen: ({ port, hostname }) => {
-      console.log(`Server started at http://${hostname}:${port}`);
-    },
-  });
+  port: 8000,
+  hostname: "0.0.0.0",
+});
